90fa62ec558b4f810fba5b5bc1e3311e22bae858
jeanlaurentstoco <jeanlaurentstoco@gmail.com>
2025-08-22 00:59:49 -0300

    Botoes da Tela Inicio

--- app/src/main/java/com/example/appedney/MainActivity.kt
+++ app/src/main/java/com/example/appedney/MainActivity.kt
@@ -16,25 +16,36 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.appedney.ui.theme.AppEdneyTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.appedney.componentes.appBar
 import com.example.appedney.ui.theme.Cor1
 import com.example.appedney.ui.theme.Cor2
+import com.example.appedney.ui.theme.Preto
 import com.example.appedney.ui.theme.fundo
+import com.example.appedney.ui.theme.fundobt
+import com.example.appedney.ui.theme.fundobt2
+import com.example.appedney.ui.theme.fundobt3
+import com.example.appedney.ui.theme.fundobt4
 
 
 class MainActivity : ComponentActivity() {
@@ -64,6 +75,146 @@ fun body(modifier: Modifier = Modifier) {
                     .background(color = fundo)
                     .fillMaxWidth(),
             ) {
+                Column {
+                    Spacer(Modifier.weight(1f))
+                    Row {
+                        Spacer(Modifier.weight(1f))
+                        Button(
+                            shape = RoundedCornerShape(24.dp),
+                            onClick = {
+
+                            },
+                            modifier = modifier
+                                .width(251.dp)
+                                .height(84.dp),
+
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = fundobt, // Background color
+                                contentColor = Color.White,  // Text/content color
+                                //disabledContainerColor = Color.LightGray, // Disabled background
+                                //disabledContentColor = Color.Gray // Disabled text/content
+                            )
+                        ) {
+                            Text(
+                                "Dados e Evolução", modifier = modifier, fontSize = 24.sp
+                            )
+                        }
+                        Spacer(Modifier.weight(0.2f))
+                        Button(
+                            shape = RoundedCornerShape(24.dp),
+                            onClick = {
+
+                            },
+                            modifier = modifier
+                                .width(251.dp)
+                                .height(84.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = fundobt2, // Background color
+                                contentColor = Color.White,  // Text/content color
+                                //disabledContainerColor = Color.LightGray, // Disabled background
+                                //disabledContentColor = Color.Gray // Disabled text/content
+                            )
+                        ) {
+                            Text(
+                                "Médias", modifier = modifier, fontSize = 24.sp
+                            )
+                        }
+                        Spacer(Modifier.weight(1f))
+                    }
+                    Spacer(Modifier.weight(0.1f))
+                    ////////////////////////////////////////////////////////////////////////////////
+                    Row {
+                        Spacer(Modifier.weight(1f))
+                        Button(
+                            shape = RoundedCornerShape(24.dp),
+                            onClick = {
+
+                            },
+                            modifier = modifier
+                                .width(251.dp)
+                                .height(84.dp),
+
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = fundobt, // Background color
+                                contentColor = Color.White,  // Text/content color
+                                //disabledContainerColor = Color.LightGray, // Disabled background
+                                //disabledContentColor = Color.Gray // Disabled text/content
+                            )
+                        ) {
+                            Text(
+                                "Etiquetas", modifier = modifier, fontSize = 24.sp
+                            )
+                        }
+                        Spacer(Modifier.weight(0.2f))
+                        Button(
+                            shape = RoundedCornerShape(24.dp),
+                            onClick = {
+
+                            },
+                            modifier = modifier
+                                .width(251.dp)
+                                .height(84.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = fundobt3, // Background color
+                                contentColor = Preto,  // Text/content color
+                                //disabledContainerColor = Color.LightGray, // Disabled background
+                                //disabledContentColor = Color.Gray // Disabled text/content
+                            )
+                        ) {
+                            Text(
+                                "Fases", modifier = modifier, fontSize = 24.sp
+                            )
+                        }
+                        Spacer(Modifier.weight(1f))
+                    }
+                    Spacer(Modifier.weight(0.1f))
+                    ////////////////////////////////////////////////////////////////////////////////
+                    Row {
+                        Spacer(Modifier.weight(1f))
+                        Button(
+                            shape = RoundedCornerShape(24.dp),
+                            onClick = {
+
+                            },
+                            modifier = modifier
+                                .width(251.dp)
+                                .height(84.dp),
+
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = fundobt, // Background color
+                                contentColor = Color.White,  // Text/content color
+                                //disabledContainerColor = Color.LightGray, // Disabled background
+                                //disabledContentColor = Color.Gray // Disabled text/content
+                            )
+                        ) {
+                            Text(
+                                "Acomp. Prensagem", modifier = modifier, fontSize = 24.sp
+                            )
+                        }
+                        Spacer(Modifier.weight(0.2f))
+                        Button(
+                            shape = RoundedCornerShape(24.dp),
+                            onClick = {
+
+                            },
+                            modifier = modifier
+                                .width(251.dp)
+                                .height(84.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = fundobt4, // Background color
+                                contentColor = Preto,  // Text/content color
+                                //disabledContainerColor = Color.LightGray, // Disabled background
+                                //disabledContentColor = Color.Gray // Disabled text/content
+                            )
+                        ) {
+                            Text(
+                                "Gráficos", modifier = modifier, fontSize = 24.sp
+                            )
+                        }
+                        Spacer(Modifier.weight(1f))
+                    }
+                    Spacer(Modifier.weight(1f))
+                }
                 NavegacaoInferior(modifier = Modifier.align(Alignment.BottomCenter))
             }
         }
